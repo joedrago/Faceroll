73763ced64468b4daaa30fdc91c3360fedd0c7b0
Joe Drago <joedrago@gmail.com>
2025-07-10 12:54:20 -0700

    More bear stuff

--- SpecCD.lua
+++ SpecCD.lua
@@ -10,6 +10,7 @@ local spec = Faceroll.createSpec("CD", "006600", "DRUID-CLASSIC")
 spec.buffs = {
     "Mark of the Wild",
     "Thorns",
+    "Rejuvenation",
 }
 
 -----------------------------------------------------------------------------------------
@@ -18,10 +19,15 @@ spec.buffs = {
 spec.states = {
     "bear",
     "targetingenemy",
+    "melee",
     "combat",
+    "manaL80",
+    "hpL90",
     "roar",
     "moonfire",
     "thorns",
+    "rejuvenation",
+    "enrage",
 }
 
 spec.calcState = function(state)
@@ -33,8 +39,26 @@ spec.calcState = function(state)
         state.targetingenemy = true
     end
 
+    if IsSpellInRange("Growl", "target") == 1 then
+        state.melee = true
+    end
+
     if UnitAffectingCombat("player") then
         state.combat = true
+    end
+
+    local curMana = UnitPower("player", 0)
+    local maxMana = UnitPowerMax("player", 0)
+    local norMana = curMana / maxMana
+    if norMana < 0.8 then
+        state.manaL80 = true
+    end
+
+    local curHP = UnitHealth("player")
+    local maxHP = UnitHealthMax("player")
+    local norHP = curHP / maxHP
+    if norHP < 0.9 then
+        state.hpL90 = true
     end
 
     if Faceroll.isDotActive("Demoralizing Roar") > 0.1 then
@@ -49,6 +73,14 @@ spec.calcState = function(state)
         state.thorns = true
     end
 
+    if Faceroll.isBuffActive("Rejuvenation") then
+        state.rejuvenation = true
+    end
+
+    if Faceroll.isSpellAvailable("Enrage") then
+        state.enrage = true
+    end
+
     return state
 end
 
@@ -61,12 +93,17 @@ spec.actions = {
     "roar",
     "maul",
     "moonfire",
+    "rejuvenation",
+    "enrage",
 }
 
 spec.calcAction = function(mode, state)
     if mode == Faceroll.MODE_ST or mode == Faceroll.MODE_AOE then
 
-        if state.targetingenemy then
+        if state.hpL90 and not state.manaL80 and not state.combat and not state.rejuvenation then
+            return "rejuvenation"
+
+        elseif state.targetingenemy then
             if not state.combat and not state.thorns then
                 return "thorns"
 
@@ -76,8 +113,14 @@ spec.calcAction = function(mode, state)
             elseif not state.bear then
                 return "bear"
 
+            elseif state.enrage then
+                return "enrage"
+
             elseif not state.roar then
-                return "roar"
+                if state.melee then
+                    return "roar"
+                end
+                -- we want to wait if we can't roar
 
             else
                 return "maul"
