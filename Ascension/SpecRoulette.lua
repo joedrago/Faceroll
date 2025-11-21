61776139e139c5c946ae7fab41bb1c87a9ce9e2e
Joe Drago <joedrago@gmail.com>
2025-11-20 22:45:27 -0800

    always roulette with Deathbringer, add duo option

--- Ascension/SpecRoulette.lua
+++ Ascension/SpecRoulette.lua
@@ -5,8 +5,11 @@ if Faceroll == nil then
     _, Faceroll = ...
 end
 
-local spec = Faceroll.createSpec("ROU", "aaffaa", "HERO-Harbinger of Flame")
-Faceroll.aliasSpec(spec, "HERO-Deathbringer")
+local spec = Faceroll.createSpec("ROU", "aaffaa", "HERO-Deathbringer")
+
+spec.options = {
+    "duo",
+}
 
 -----------------------------------------------------------------------------------------
 -- States
@@ -14,6 +17,7 @@ Faceroll.aliasSpec(spec, "HERO-Deathbringer")
 spec.overlay = Faceroll.createOverlay({
     "- Combat -",
     "autoshot",
+    "duo",
 })
 
 spec.calcState = function(state)
@@ -33,7 +37,7 @@ spec.actions = {
 
 spec.calcAction = function(mode, state)
     if state.targetingenemy then
-        if not state.combat then
+        if not state.combat and not state.duo then
             return "sic"
         elseif not state.autoshot then
             return "attack"
