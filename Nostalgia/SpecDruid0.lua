fb5aba684a68e83c051b193c4bdfc10b7206c941
Joe Drago <joedrago@gmail.com>
2026-04-01 20:18:47 -0700

    Implement Druid0

--- Nostalgia/SpecDruid0.lua
+++ Nostalgia/SpecDruid0.lua
@@ -17,7 +17,17 @@ spec.macros = {
 -- States
 
 spec.overlay = Faceroll.createOverlay({
-    -- { "s_spellname", "Spell Name" },
+    "- Buffs -",
+    { "b_motw",      "Mark of the Wild" },
+    { "b_thorns",    "Thorns" },
+    { "b_rejuv",     "Rejuvenation" },
+    { "b_drink",     "Drink" },
+
+    "- Debuffs -",
+    { "d_moonfire",  "Moonfire" },
+
+    "- Spells -",
+    { "s_moonfire",  "Moonfire" },
 })
 
 spec.calcState = function(state)
@@ -28,14 +38,41 @@ end
 -- Actions
 
 spec.actions = {
-    { "wrath", spell = "Wrath" },
+    { "wrath",       spell = "Wrath" },
+    { "moonfire",    spell = "Moonfire" },
+    { "motw",        spell = "Mark of the Wild" },
+    { "thorns",      spell = "Thorns" },
+    { "rejuv",       spell = "Rejuvenation" },
+    "drink",
 }
 
 spec.calcAction = function(mode, state)
-    local st = (mode == Faceroll.MODE_ST)
     local aoe = (mode == Faceroll.MODE_AOE)
 
-    if state.targetingenemy then
-        return "wrath"
+    -- Keep Mark of the Wild up
+    if not state.b_motw and Faceroll.isActionAvailable("motw") then
+        return "motw"
+
+    -- Keep Thorns up
+    elseif not state.b_thorns and Faceroll.isActionAvailable("thorns") then
+        return "thorns"
+
+    -- Self-heal with Rejuvenation when solo and low HP
+    elseif not state.combat and not state.group and state.hp < 0.6 and not state.b_rejuv and Faceroll.isActionAvailable("rejuv") then
+        return "rejuv"
+
+    -- Drink when low mana
+    elseif state.mana < 0.9 and not state.combat and not state.b_drink and Faceroll.isActionAvailable("drink") then
+        return "drink"
+
+    elseif state.targetingenemy then
+        -- Apply Moonfire if not on target
+        if not state.d_moonfire and state.s_moonfire then
+            return "moonfire"
+
+        -- Wrath filler
+        else
+            return "wrath"
+        end
     end
 end
